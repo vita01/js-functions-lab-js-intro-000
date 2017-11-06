ed6206f9275efbd236805ec400a7f4e3c3e86037
Victoria <vitalina27@gmail.com>
2017-11-06 09:38:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 function happyHolidaysTo(name){
-return "Happy holidays, "+name +""!";
+return "Happy holidays, "+name +"!";
 
 }
 function happyHolidayTo(holiday,name){
